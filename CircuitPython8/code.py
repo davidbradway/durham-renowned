fe36d633cbc66fd31d8c1a0076c5d5246e3784a0
David Bradway <dpb6@duke.edu>
2023-05-27 13:28:34 -0400

    Replace code.py

--- CircuitPython8/code.py
+++ CircuitPython8/code.py
@@ -28,7 +28,7 @@ while True:
     pixels.show()
     # Increase or decrease to change the speed of the solid color change.
     time.sleep(5)
-    '''
+    ''' 
     '''
     for i in range(5):
         rainbow_cycle(0.01)  # Increase the number to slow down the rainbow
